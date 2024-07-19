5069dee4124cb08dc661c9d8eef5b6d629c79935
santi <santi2811b@gmail.com>
2024-07-19 12:07:39 -0600

    added records and started register endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -11,11 +11,22 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
+        Spark.post("/user",this::registerUser);
+
+
+                //start register endpoint here
+        //see if I can convert to Json an object
+
+
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
         Spark.awaitInitialization();
         return Spark.port();
+    }
+
+    private Object registerUser(Request request, Response response) {
+        return null;
     }
 
     public void stop() {
